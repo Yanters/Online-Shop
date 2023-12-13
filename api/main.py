ea0d260251d31ab214ab79a88227ffd38ab71893
Przemek <przemek.szumczyk@axbit.com>
2023-12-13 03:57:43 +0100

    Prepare template for api. With working loading categories.

--- api/main.py
+++ api/main.py
@@ -1,4 +1,6 @@
+import io
 import json
+import os
 from prestapyt import PrestaShopWebServiceDict
 
 DEFAULT_LINK = "http://localhost:8080/api/"
@@ -32,10 +34,6 @@ def load_category(name: str, parent_id: int = None):
     })
 
     if not category["categories"]:
-        category_schema = prestashop.get('categories', options={
-            'schema': 'blank'
-        })
-
         category_schema['category']['name']['language']['value'] = name
         category_schema['category']['id_parent'] = parent_id or '2'
         category_schema["category"]["active"] = 1
@@ -62,8 +60,75 @@ def delete_all_categories():
                 pass
             finally:
                 print(f"Deleted category {category_id}")
-            
+
+def load_products():
+    with open("../results/products.json") as file:
+        products = json.load(file)
+
+    print("\n\n#######################")
+    print("#                     #")
+    print("#  Adding products    #")
+    print("#                     #")
+    print("#######################\n")
+    for product in products:
+        product_id = load_product(product)
+        # load_product_images(product_id, product["images"])
+
+def load_product(product):
+    try:
+        default_category = prestashop.get("categories", options={
+            "filter[name]": product["categories"][0]
+        })
+        default_category_id = default_category["categories"]["category"]["attrs"]["id"]
+        product_schema['product']['id_category_default'] = default_category_id
+        product_schema['product']['name']['language']['value'] = product["name"]
+        product_schema["product"]["id_shop_default"] = 1
+        product_schema["product"]["reference"] = product["sku"]
+        product_schema["product"]["id_tax_rules_group"] = 1
+        product_schema["product"]["indexed"] = 1
+        product_schema['product']['price'] = round(float(product["price"])/1.23, 2)
+        product_schema['product']['description']['language']['value'] = product["description"]
+        product_schema['product']['description_short']['language']['value'] = product["short_description"]
+        product_schema["product"]["active"] = 1
+        product_schema["product"]["state"] = 1
+        product_schema["product"]["visibility"] = 1
+        product_schema["product"]["available_for_order"] = 1
+        product_schema["product"]["minimal_quantity"] = 1
+        product_schema["product"]["show_price"] = 1
+        response = prestashop.add('products', product_schema)
+        return response['prestashop']['product']['id']
+    # except Exception as e:
+    #     print(e)
+    # else:
+    #     print(f"Added product {product['name']}")
+    finally:
+        print("Finished adding products")
+
+def load_product_images(product_id: int, images_names: [str]):
+    for image_name in images_names:
+        image_path = f"../results/images/{image_name}"
+        if os.path.exists(image_path):
+            file = io.open(image_path, 'rb')
+            image_data = file.read()
+            file.close()
+            prestashop.add(f"images/products/{product_id}", files=[('image', image_name, image_data)])
+            print(f"Added image {image_name} to product {product_id}")
 
 if __name__ == "__main__":
     prestashop = PrestaShopWebServiceDict(DEFAULT_LINK, API_KEY)
-    load_categories()
+
+    category_schema = prestashop.get('categories', options={
+        'schema': 'blank'
+    })
+
+    product_schema = prestashop.get('products', options={
+        'schema': 'blank'
+    })
+    print(product_schema)
+
+    image_schema = prestashop.get('images/products', options={
+        'schema': 'blank'
+    })
+
+    # load_categories()
+    load_products()
